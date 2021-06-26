c8e5dd700c18352c5cad080ff18cd630085464f8
Mainv4 <marin@LAPTOP-FHMRS9D4.localdomain>
2021-06-26 11:15:29 +0200

    Modified guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,2 +1,25 @@
-#!/bin/bash
-echo "Hello Word"
+#!/bin/bash 
+how_many_files(){
+    answer=$(ls -A|wc -l)
+    while true;
+    do
+        echo "How many files in the current directory?"
+        read  guess
+        if [[ $guess ]] && [ $guess -eq $guess 2>/dev/null ]
+            then
+                if [ $guess -lt $answer ]
+                then
+                    echo "Too low..."
+                continue;
+                elif [ $guess -gt $answer ]
+                then
+                    echo "Too high..."
+                continue;
+                else
+                    echo "Yes, this is true!!!!"
+		break
+                fi
+        fi
+    done
+}
+how_many_files
